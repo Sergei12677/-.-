2c4812fffdb8fa9a6b952fb5347e6f6ceb670523
Sergei12677 <gorod_26@icloud.com>
2024-10-06 15:56:52 +0500

    Add files via upload

---
+++ mobile_1_4.py
@@ -0,0 +1,7 @@
+#my_string = input('Sergei')
+print('my_string'.upper())
+print('my_string'.lower())
+print('my_string'.replace('_',''))
+example = 'my_string'
+print(example[0])
+print(example[-1])
